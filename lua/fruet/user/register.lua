510a5f5d87d963f5dd350f5cf03e93c50cd8e017
gabrielfruet <gabrielfruet538@gmail.com>
2024-08-21 20:47:19 -0300

    fix: correct the logic of validating a register

--- lua/fruet/user/register.lua
+++ lua/fruet/user/register.lua
@@ -1,10 +1,23 @@
 local M = {}
 
+M.registers = {
+    '"', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', ':', '.', '/',
+    '*', '+', '=', '_', '~', '#', '%', '\"', '^', '@', '!', '>', '<', '{', '}',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+}
+
+
 M.run  = function ()
+
+    local register_by_byte = {}
+    for _, reg in pairs(M.registers) do
+        register_by_byte[string.byte(reg)] = true
+    end
 
     local function get_register()
         local char = vim.fn.getcharstr()
-        if string.byte(char) == 27 then
+        if not register_by_byte[string.byte(char)] then
             return nil
         end
         return char
@@ -22,7 +35,11 @@ M.run  = function ()
             prompt = string.format('Edit the register \'%s\'', reg),
             default = regcontent,
         }, function (input)
-            vim.fn.setreg(reg, input)
+            if not input then return end
+            local ok, _ = pcall(vim.fn.setreg, reg, input)
+            if not ok then
+                vim.print(string.format('No register named \'%s\'', reg))
+            end
         end)
 
     end
