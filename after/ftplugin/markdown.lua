71eb941e9b68d6c3981309caa29634711988cde8
gabrielfruet <gabrielfruet538@gmail.com>
2024-09-22 08:28:39 -0300

    feat: added error handling to pandoc hot reload

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -27,11 +27,26 @@ end, {
         noremap=true,
     })
 
+local function stder_handler(cmd)
+    return function (job_id, data, event)
+        -- Check if there's any data in the stderr output
+        if data and #data > 1 then
+            -- Join the data array (it may come in chunks) and print to the user
+            local error_message = table.concat(data, "\n")
+            -- Print the error message
+            vim.api.nvim_err_writeln("Error from \'" .. cmd .. "\' " .. job_id .. ": " .. error_message)
+        end
+    end
+end
+
 vim.api.nvim_create_autocmd('BufWritePost', {
     group=vim.api.nvim_create_augroup('hot_reload_to_pandoc', {clear=true}),
     callback=function ()
         if not hot_reload then return end
-
-        vim.fn.jobstart(string.format('pandoc %s.md -o %s.pdf', fpath_wo_extension, fpath_wo_extension), {detach=true})
+        local cmd = string.format('pandoc %s.md -o %s.pdf', fpath_wo_extension, fpath_wo_extension)
+        vim.fn.jobstart(cmd, {
+            detach=true,
+            on_stderr=stder_handler(cmd)
+        })
     end
 })
