ee10aa268f86b5eb4237ce83eeb25903e0c45014
gabrielfruet <gabrielfruet538@gmail.com>
2024-11-14 12:31:03 -0300

    some changes to markdown ftplugin

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,7 +1,10 @@
+_G.pandoc_buffers = _G.pandoc_buffers or {}
+
 local buf = vim.api.nvim_get_current_buf()
 local fpath_wo_extension = vim.fn.expand('%:r')
 
 local kset = vim.keymap.set
+local hot_reload = false
 
 kset('n', '<leader>zo', function ()
     vim.fn.jobstart(string.format('zathura %s.pdf', fpath_wo_extension), {detach=true})
@@ -10,8 +13,6 @@ end, {
         noremap=true,
     })
 
-
-local hot_reload = false
 
 kset('n', '<leader>re', function ()
     hot_reload = true
@@ -29,6 +30,33 @@ end, {
         noremap=true,
     })
 
+local function generate_toc_quickfix()
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    local quickfix_list = {}
+
+    for i, line in ipairs(lines) do
+        local level, header = line:match("^(#+)%s+(.*)")
+        if level and header then
+            local indent_level = string.rep("  ", #level - 1)
+            table.insert(quickfix_list, {
+                bufnr = 0,
+                lnum = i + 1,
+                col = 1,
+                text = indent_level .. header,
+            })
+        end
+    end
+
+    if #quickfix_list > 0 then
+        vim.fn.setqflist(quickfix_list, "r")
+        vim.cmd("copen")
+    else
+        print("No headers found to create Table of Contents.")
+    end
+end
+
+vim.api.nvim_create_user_command("PandocToc", generate_toc_quickfix, {})
+
 local function stder_handler(cmd)
     return function (job_id, data, event)
         -- Check if there's any data in the stderr output
@@ -43,6 +71,7 @@ end
 
 vim.api.nvim_create_autocmd('BufWritePost', {
     group=vim.api.nvim_create_augroup('hot_reload_to_pandoc', {clear=true}),
+    buffer=buf,
     callback=function ()
         if not hot_reload then return end
         local cmd = string.format('pandoc %s.md -o %s.pdf', fpath_wo_extension, fpath_wo_extension)
