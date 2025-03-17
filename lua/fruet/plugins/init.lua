16e9b4272c15b18d9150682a0240f7ad6c38656a
gabrielfruet <gabrielfruet538@gmail.com>
2025-03-17 17:53:17 -0300

    feat: image

--- lua/fruet/plugins/init.lua
+++ lua/fruet/plugins/init.lua
@@ -64,6 +64,11 @@ return {
         end
     },
     {
+        "3rd/image.nvim",
+        build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+        opts = {}
+    },
+    {
         "zbirenbaum/copilot.lua",
         event = "InsertEnter",
         config = function()
