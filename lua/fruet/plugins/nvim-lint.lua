5e00dbb4482f4ad3bb29928b6f810a4ae1d51f50
gabrielfruet <gabrielfruet538@gmail.com>
2025-05-28 11:19:51 -0300

    disable linting

--- lua/fruet/plugins/nvim-lint.lua
+++ lua/fruet/plugins/nvim-lint.lua
@@ -5,6 +5,7 @@ return {
         "BufNewFile"
     },
     config = function ()
+        local lint_enabled = true
         local nvim_lint = require('lint')
         nvim_lint.linters_by_ft = {
             python={'ruff'},
@@ -14,18 +15,31 @@ return {
             make={'checkmake'},
         }
         local linaugroup = vim.api.nvim_create_augroup("Linting", {clear = true})
+        local function try_lint()
+            if lint_enabled then
+                nvim_lint.try_lint()
+            end
+        end
         vim.api.nvim_create_autocmd({ "BufWritePost" , "BufEnter", "InsertLeave"}, {
             group = linaugroup,
             callback = function()
-                nvim_lint.try_lint()
+                try_lint()
             end,
         })
         vim.api.nvim_create_user_command("Lint",function()
-                nvim_lint.try_lint()
-            end, {}
+            try_lint()
+        end, {}
+        )
+        vim.api.nvim_create_user_command("LintDisable",function()
+            lint_enabled = false
+        end, {}
+        )
+        vim.api.nvim_create_user_command("LintEnable",function()
+            lint_enabled = true
+        end, {}
         )
         vim.keymap.set('n', '<leader>li', function ()
-            nvim_lint.try_lint()
+            try_lint()
         end, {noremap=true})
 
         -- Luacheck with global vim
