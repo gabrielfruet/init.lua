8753c22e5be69c3fc56718dd7aa7bc74a6d02152
gabrielfruet <gabrielfruet538@gmail.com>
2025-07-23 16:16:55 -0300

    adding formating and fixing from ruff

--- lua/fruet/plugins/conform.lua
+++ lua/fruet/plugins/conform.lua
@@ -1,6 +1,9 @@
 return {
     'stevearc/conform.nvim',
     opts = {
+        formatters_by_ft = {
+            python = { "ruff_fix", "ruff_format" },
+        },
         format_on_save = {
             lsp_fallback = false,
             timeout_ms = 1000,
